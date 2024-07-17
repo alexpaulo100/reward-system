5b2b11b278bc69715d9bc144981323640889b828
Alex Silva <alex.paulo100@gmail.com>
2024-07-16 21:00:41 -0300

    Set up project initial organization and metadata

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages # type: ignore
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward Point System",
+    author="Alex Silva",
+    packages=find_packages(),
+)
