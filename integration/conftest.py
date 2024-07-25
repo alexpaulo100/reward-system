e48c2d964be8e61cc95cfb7af3c44c4b946e5044
Alex Paulo Pereira Silva <alex.paulo100@gmail.com>
2024-07-25 12:06:08 -0300

    Organized tests (#1)
    * Organized tests
    
    * Add CI GHA
    
    * AT CI GHA
    
    * ATT CI GHA
    
    * Add integration
    
    * Alt integration
    
    * add version
    
    * version

---
+++ integration/conftest.py
@@ -0,0 +1,16 @@
+MARKER = """\
+unit: Mark integration tests
+high: High Priority
+medium: Medium Priority
+low: Low Priority
+"""
+
+
+def pytest_configure(config):
+    map(lambda line: config.addinivalue_line('markers', line), MARKER.split("\n"))
+
+
+
+
+
+
