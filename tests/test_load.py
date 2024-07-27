0886669b77ace26cc96240beda3e923334dc3dbc
Alex Silva <alex.paulo100@gmail.com>
2024-07-27 13:40:08 -0300

    Fix error

--- tests/test_load.py
+++ tests/test_load.py
@@ -5,35 +5,17 @@ from dundie.core import load
 from .constants import PEOPLE_FILE
 
 
-@pytest.fixture(scope="function", autouse=True)
-def create_new_file(tmpdir):
-    file_ = tmpdir.join("new_file.txt")
-    file_.write("Isso é sujeira...")
-    yield
-    file_.remove()
-
-    
 @pytest.mark.unit
 @pytest.mark.high
-def test_load(request):
+def test_load_positive_has_2_people(request):
     """Test load function."""
-    filepath = f"arquivo_indesejado-{uuid.uuid4()}.txt"
-    request.addfinalizer(lambda: os.unlink(filepath))
-    
-
-    assert len(load(PEOPLE_FILE)) == 2
-    assert load(PEOPLE_FILE)[0][0] == 'J'
+    assert len(load(PEOPLE_FILE)) == 3
 
 
 @pytest.mark.unit
 @pytest.mark.high
-def test_load2():
+def test_load_positive_first_name_starts_with_j(request):
     """Test load function."""
-
-    with open("arquivo_indesejado.txt", "w") as file_:
-        file_.write("dados uteis somente para teste")
-        
-    assert len(load(PEOPLE_FILE)) == 2
     assert load(PEOPLE_FILE)[0][0] == 'J'
 
     
